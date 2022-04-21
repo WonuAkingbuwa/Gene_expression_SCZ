cf81547ae9e81716ac6b3dea80f65613f1f34c95
Wonuola Akingbuwa <34710305+WonuAkingbuwa@users.noreply.github.com>
2022-04-21 12:00:40 +0200

    Add files via upload

---
+++ (ultra-)rare variants/QC/19_loop_rv_ptv_geneset_association_tests_fin_exclusion.R
@@ -0,0 +1,253 @@
+library(data.table)
+
+files <- list.files("/media/veracrypt10/Analyses/annotations/RVs_genesets/")
+common <- fread(files[1], sep='\t', header=TRUE, data.table=FALSE, select = c(1,4:6,38,70:79))    # columns that are common to all files 
+readfun <- function(fn) {
+  x <- fread(fn, sep='\t', header=TRUE, data.table=FALSE, select = c(80:88))
+  colnames(x) <- paste0(colnames(x), paste0("__",gsub('\\.', '_', fn)))
+  return(x)
+}
+df <- do.call(cbind,lapply(files,readfun))
+df <- cbind(common,df)
+
+#check sex information is accurate 
+table(df$imputesex.impute_sex.is_female)
+table(df$phenotype.SEX)
+
+#import overall RV file to use to for exclusions
+#import overall RV counts
+df_rv <- fread('/media/veracrypt10/Analyses/annotations/RVs_after_QC.tsv', sep='\t', header=TRUE, data.table=FALSE)
+df_rv$n_RV <- df_rv$n_RV_SNP + df_rv$n_RV_indel
+df_rv <- df_rv[c(1,80:89)]
+
+#merge both files 
+mergeddata_rv <- merge(df, df_rv, by='s') 
+
+#import Finnish exclusion list 
+exclude_fin <- fread("/media/veracrypt10/Analyses/Analyses/finnish_pca.remove_sample_list")
+mergeddata_rv <- subset(mergeddata_rv, !(mergeddata_rv$s %in% exclude_fin$V1))
+
+  #add dummy variable outcome column coded as 1 or 0
+mergeddata_rv$outcome <- ifelse(mergeddata_rv$phenotype.ANALYSIS_CAT == "Case", 1, 0)
+
+#filter to just people with schizophrenia 
+mergeddata_rv <- subset(mergeddata_rv, mergeddata_rv$phenotype.PRIMARY_DISEASE == "Control" | mergeddata_rv$phenotype.PRIMARY_DISEASE == "Schizophrenia") 
+
+#exclude individuals with more than 4 median absolute deviations from the study and ethnicity-specific median number of rare variants 
+if (mad(mergeddata_rv$n_RV) == 0 )
+{
+  mergeddata_rv <- mergeddata_rv[(mergeddata_rv$n_RV < median(mergeddata_rv$n_RV) + 4*mad(mergeddata_rv$n_RV)) & (mergeddata_rv$n_RV > median(mergeddata_rv$n_RV) - 4*mad(mergeddata_rv$n_RV)),]
+  
+  print(paste0("N after exclusions:",nrow(mergeddata_rv)))
+} else
+{
+  mergeddata_rv <- mergeddata_rv[(mergeddata_rv$n_RV < median(mergeddata_rv$n_RV) + 4*sd(mergeddata_rv$n_RV)) & (mergeddata_rv$n_RV > median(mergeddata_rv$n_RV) - 4*sd(mergeddata_rv$n_RV)),]
+  
+  print(paste0("N after exclusions:",nrow(mergeddata_rv)))
+} 
+
+#subset - include other variables and covariates 
+mergeddata_rv_ptv <- subset(mergeddata_rv[,c("s","phenotype.SEX","outcome",colnames(mergeddata_rv)[grep("n_RV_PTV|pca.PC",colnames(mergeddata_rv))])])
+mergeddata_rv_syn <- subset(mergeddata_rv[,c("s","phenotype.SEX","outcome",colnames(mergeddata_rv)[grep("n_RV_synonymous|pca.PC",colnames(mergeddata_rv))])])
+
+#RV PTVs####
+#run loop for all ptv variant burden scores across all genesets 
+#outcome
+out_start=3
+out_end=3
+out_nvar=out_end-out_start+1
+
+out_variable=rep(NA, out_nvar)
+out_beta=rep(NA, out_nvar)
+out_se = rep(NA, out_nvar)
+out_z = rep(NA, out_nvar)
+out_OR = rep(NA, out_nvar)
+out_p=rep(NA, out_nvar)
+
+# exposure
+exp_start=14
+exp_end=125
+exp_nvar=exp_end-exp_start+1
+
+exp_variable=rep(NA, exp_nvar)
+exp_beta=rep(NA, exp_nvar)
+exp_se = rep(NA, out_nvar)
+exp_z = rep(NA, out_nvar)
+exp_OR = rep(NA, out_nvar)
+exp_p=rep(NA, exp_nvar)
+
+number=1
+
+for (i in out_start:out_end) {
+  outcome = colnames(mergeddata_rv_ptv)[i]
+  for (j in exp_start:exp_end){
+    exposure = colnames(mergeddata_rv_ptv)[j]
+    x <- glm(outcome ~ get(exposure)+ n_RV_PTV + pca.PC1 + pca.PC2 + pca.PC3 + pca.PC4 + pca.PC5 + pca.PC6 + pca.PC7 + pca.PC8 + pca.PC9 + pca.PC10 + phenotype.SEX, data = mergeddata_rv_ptv, family = binomial)
+    beta <- summary(x)$coefficients[,1]
+    se <- summary(x)$coefficients[,2]
+    z <- summary(x)$coefficients[,3]
+    OR <- exp(beta)
+    p <- summary(x)$coefficients[,4]
+    
+    out_beta[number] <- as.numeric(beta[2])
+    out_se[number] <- as.numeric(se[2])
+    out_z[number] <- as.numeric(z[2])
+    out_OR[number] <- as.numeric(OR[2])
+    out_p[number] <- as.numeric(p[2])
+    out_variable[number] <- outcome
+    number <- number + 1
+    
+    exp_beta[number] <- as.numeric(beta[2])
+    exp_se[number] <- as.numeric(se[2])
+    exp_z[number] <- as.numeric(z[2])
+    exp_OR[number] <- as.numeric(OR[2])
+    exp_p[number] <- as.numeric(p[2])
+    exp_variable[number] <- exposure
+    number <- number + 1
+    
+  }
+}
+
+
+outcome <- data.frame(out_variable, out_beta, out_se, out_z, out_OR, out_p)
+exposure <- data.frame(exp_variable, exp_beta, exp_se, exp_z, exp_OR, exp_p)
+
+#manage dataframes
+library(tidyverse)
+
+outcome <- outcome %>% 
+  rename(
+    variable = out_variable,
+    beta = out_beta,
+    se = out_se,
+    z = out_z,
+    OR = out_OR,
+    p = out_p,
+  )
+exposure <- exposure %>% 
+  rename(
+    variable = exp_variable,
+    beta = exp_beta,
+    se = exp_se,
+    z = exp_z,
+    OR = exp_OR,
+    p = exp_p,
+  )
+all <- rbind(outcome, exposure)
+all <- na.omit(all)
+
+#correct dataframe
+dv <- all[c(1:112),]
+iv <- all[c(113:224),]
+dv$iv <- iv$variable
+
+dv <- dv[c("variable", "iv", "beta", "se", "z", "OR", "p")]
+dv$fdr_p <- p.adjust(dv$p, method = "fdr")
+dv$significant <- ifelse(dv$fdr_p < 0.05, "yes", "no")
+dv$outcome <- "Schizophrenia"
+names(dv)[2] <- "geneset"
+dv <- dv[c(10,2,3,4,5,6,7,8,9)]
+table(dv$significant)
+
+write.table(dv, file = "/media/veracrypt10/Analyses/annotations/scz_rv_ptv_geneset_estimates_FIN_exclusions.tsv", sep="\t",dec = ".", row.names = F )
+
+#RV synonymous variants####
+#run loop for all synonymous variant burden scores across all genesets 
+#outcome
+out_start=3
+out_end=3
+out_nvar=out_end-out_start+1
+
+out_variable=rep(NA, out_nvar)
+out_beta=rep(NA, out_nvar)
+out_se = rep(NA, out_nvar)
+out_z = rep(NA, out_nvar)
+out_OR = rep(NA, out_nvar)
+out_p=rep(NA, out_nvar)
+
+# exposure
+exp_start=14
+exp_end=125
+exp_nvar=exp_end-exp_start+1
+
+exp_variable=rep(NA, exp_nvar)
+exp_beta=rep(NA, exp_nvar)
+exp_se = rep(NA, out_nvar)
+exp_z = rep(NA, out_nvar)
+exp_OR = rep(NA, out_nvar)
+exp_p=rep(NA, exp_nvar)
+
+number=1
+
+for (i in out_start:out_end) {
+  outcome = colnames(mergeddata_rv_syn)[i]
+  for (j in exp_start:exp_end){
+    exposure = colnames(mergeddata_rv_syn)[j]
+    x <- glm(outcome ~ get(exposure)+ n_RV_synonymous + pca.PC1 + pca.PC2 + pca.PC3 + pca.PC4 + pca.PC5 + pca.PC6 + pca.PC7 + pca.PC8 + pca.PC9 + pca.PC10 + phenotype.SEX, data = mergeddata_rv_syn, family = binomial)
+    beta <- summary(x)$coefficients[,1]
+    se <- summary(x)$coefficients[,2]
+    z <- summary(x)$coefficients[,3]
+    OR <- exp(beta)
+    p <- summary(x)$coefficients[,4]
+    
+    out_beta[number] <- as.numeric(beta[2])
+    out_se[number] <- as.numeric(se[2])
+    out_z[number] <- as.numeric(z[2])
+    out_OR[number] <- as.numeric(OR[2])
+    out_p[number] <- as.numeric(p[2])
+    out_variable[number] <- outcome
+    number <- number + 1
+    
+    exp_beta[number] <- as.numeric(beta[2])
+    exp_se[number] <- as.numeric(se[2])
+    exp_z[number] <- as.numeric(z[2])
+    exp_OR[number] <- as.numeric(OR[2])
+    exp_p[number] <- as.numeric(p[2])
+    exp_variable[number] <- exposure
+    number <- number + 1
+    
+  }
+}
+
+
+outcome <- data.frame(out_variable, out_beta, out_se, out_z, out_OR, out_p)
+exposure <- data.frame(exp_variable, exp_beta, exp_se, exp_z, exp_OR, exp_p)
+
+#manage dataframes
+library(tidyverse)
+
+outcome <- outcome %>% 
+  rename(
+    variable = out_variable,
+    beta = out_beta,
+    se = out_se,
+    z = out_z,
+    OR = out_OR,
+    p = out_p,
+  )
+exposure <- exposure %>% 
+  rename(
+    variable = exp_variable,
+    beta = exp_beta,
+    se = exp_se,
+    z = exp_z,
+    OR = exp_OR,
+    p = exp_p,
+  )
+all <- rbind(outcome, exposure)
+all <- na.omit(all)
+
+#correct dataframe
+dv <- all[c(1:112),]
+iv <- all[c(113:224),]
+dv$iv <- iv$variable
+
+dv <- dv[c("variable", "iv", "beta", "se", "z", "OR", "p")]
+dv$fdr_p <- p.adjust(dv$p, method = "fdr")
+dv$significant <- ifelse(dv$fdr_p < 0.05, "yes", "no")
+dv$outcome <- "Schizophrenia"
+names(dv)[2] <- "geneset"
+dv <- dv[c(10,2,3,4,5,6,7,8,9)]
+table(dv$significant)
+
+write.table(dv, file = "/media/veracrypt10/Analyses/annotations/scz_rv_syn_geneset_estimates_FIN_exclusions.tsv", sep="\t",dec = ".", row.names = F )
---
+++ (ultra-)rare variants/QC/19_pca_exclusions_plot_urv.R
@@ -0,0 +1,59 @@
+library(data.table)
+library(ggplot2)
+library(reshape2)
+
+#ultra-rare PTVs####
+#import geneset regression estimates (total + exclusions) + add data identifiers to the header names
+ptv_tot_estimates <- fread("scz_urv_ptv_geneset_estimates_ewptvcov.tsv", sep='\t', header=TRUE, data.table=FALSE)
+ptv_tot_estimates$geneset <- gsub("n_URV_PTV__URVs_|_intervals_bed_tsv", "", ptv_tot_estimates$geneset)
+ptv_tot_estimates <- ptv_tot_estimates[,c(2:9)]
+colnames(ptv_tot_estimates)[2:8] <- paste("tot", colnames(ptv_tot_estimates[,c(2:8)]), sep = "_")
+
+ptv_FIN_estimates <- fread("scz_urv_ptv_geneset_estimates_FIN_exclusions.tsv", sep='\t', header=TRUE, data.table=FALSE)
+ptv_FIN_estimates$geneset <- gsub("n_URV_PTV__URVs_|_intervals_bed_tsv", "", ptv_FIN_estimates$geneset)
+ptv_FIN_estimates <- ptv_FIN_estimates[,c(2:9)]
+colnames(ptv_FIN_estimates)[2:8] <- paste("fin", colnames(ptv_FIN_estimates[,c(2:8)]), sep = "_")
+
+ptv_NorSwe_estimates <- fread("scz_urv_ptv_geneset_estimates_NorSwe_exclusions.tsv", sep='\t', header=TRUE, data.table=FALSE)
+ptv_NorSwe_estimates$geneset <- gsub("n_URV_PTV__URVs_|_intervals_bed_tsv", "", ptv_NorSwe_estimates$geneset)
+ptv_NorSwe_estimates <- ptv_NorSwe_estimates[,c(2:9)]
+colnames(ptv_NorSwe_estimates)[2:8] <- paste("nor", colnames(ptv_NorSwe_estimates[,c(2:8)]), sep = "_")
+
+ptv_othEUR_estimates <- fread("scz_urv_ptv_geneset_estimates_othEUR_exclusions.tsv", sep='\t', header=TRUE, data.table=FALSE)
+ptv_othEUR_estimates$geneset <- gsub("n_URV_PTV__URVs_|_intervals_bed_tsv", "", ptv_othEUR_estimates$geneset)
+ptv_othEUR_estimates <- ptv_othEUR_estimates[,c(2:9)]
+colnames(ptv_othEUR_estimates)[2:8] <- paste("oth", colnames(ptv_othEUR_estimates[,c(2:8)]), sep = "_")
+
+ptv_allEURout_estimates <- fread("scz_urv_ptv_geneset_estimates_allEURout_exclusions.tsv", sep='\t', header=TRUE, data.table=FALSE)
+ptv_allEURout_estimates$geneset <- gsub("n_URV_PTV__URVs_|_intervals_bed_tsv", "", ptv_allEURout_estimates$geneset)
+ptv_allEURout_estimates <- ptv_allEURout_estimates[,c(2:9)]
+colnames(ptv_allEURout_estimates)[2:8] <- paste("all", colnames(ptv_allEURout_estimates[,c(2:8)]), sep = "_")
+
+#merge data
+all_estimates <- merge(ptv_tot_estimates, ptv_FIN_estimates, by = "geneset")
+all_estimates <- merge(all_estimates, ptv_NorSwe_estimates, by = "geneset")
+all_estimates <- merge(all_estimates, ptv_othEUR_estimates, by = "geneset")
+all_estimates <- merge(all_estimates, ptv_allEURout_estimates, by = "geneset")
+all_estimates <- all_estimates[,c(1,2,9,16,23,30)]
+
+#plot
+tiff("pca_exclusions_plot_urv_ptv.tiff", width = 3000, height = 2000, res=600)
+  ggplot(all_estimates, aes(tot_beta)) +
+  geom_point(aes(y=fin_beta, colour="Finnish"), size=0.5) +
+  geom_point(aes(y=nor_beta, colour="Northern Swedish"), size=0.5) +
+  geom_point(aes(y=oth_beta, colour="Other European"), size=0.5) +
+  geom_point(aes(y=all_beta, colour="All outliers"), size=0.5) +
+  
+  ylab("Beta (Samples excluding outlier populations)") +
+  xlab("Beta (total European sample)") +
+  theme(axis.title.x = element_text(size=6)) +
+  theme(axis.title.y = element_text(size=6)) +
+  theme(axis.text.x = element_text(size=5)) +
+  theme(axis.text.y = element_text(size=5)) +
+    
+  labs(colour="Populations excluded") +
+  theme(legend.text = element_text(size = 4)) +
+  theme(legend.title = element_text(size = 5))
+    
+  dev.off()
+  
